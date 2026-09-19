b757e2d6851130a829199218878691f33880d8d5
Le Huu Huy <lehuuhuy211405@gmail.com>
2026-09-19 11:08:55 +0700

    feat: add structure and entity for payment service , booking service

---
+++ paymentservice/src/main/java/com/cgv/paymentservice/entity/OutboxEvent.java
@@ -0,0 +1,44 @@
+package com.cgv.bookingservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "outbox_booking_events",
+        indexes = {
+                @Index(name = "idx_cgv_outbox_created_at_is_published" , columnList = "is_published , created_at"),
+                @Index(name = "idx_cgv_outbox_aggregate_id" , columnList = "aggregate_id")
+        }
+)
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OutboxEvent extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @Column(nullable = false)
+    String aggregateId;
+
+    @Column(nullable = false)
+    String aggregateType;
+
+    @Column(nullable = false)
+    String eventType;
+
+    @Column(name = "payload" , columnDefinition = "TEXT" , nullable = false)
+    String payload;
+
+    @Column
+    Boolean isPublished;
+
+    @Column
+    @Builder.Default
+    Integer retryCount = 0;
+}
